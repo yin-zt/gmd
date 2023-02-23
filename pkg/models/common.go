a8b22c0318375d624747df840f1855cb6dad8a21
yin-zt <15626499421@163.com>
2023-02-24 00:25:37 +0800

    1. 在本地起一个ftp服务端，将本地的目录通过ftp协议分享出去

--- pkg/models/common.go
+++ pkg/models/common.go
@@ -1,7 +1,21 @@
 package models
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	log "github.com/cihub/seelog"
+	"io"
+	"io/ioutil"
+	"net"
 	"os"
+	"os/exec"
+	"os/user"
+	"regexp"
+	"runtime"
 	"strings"
 )
 
@@ -39,6 +53,277 @@ func (this *Common) GetAction() string {
 		return os.Args[1]
 	} else {
 		return "help"
+	}
+
+}
+
+// GetArgsMap 将位置参数转换为字典，形如：./main.go -f aa -b --back ok --file file1
+// 会被转换为 map[string]string{"f": "aa", "b": 1, "back": "ok", "file": "file1"}
+// 如果--file 参数，会将file1内容读取出来存入file这个键中
+// 传入的位置参数中val值最后不能带"$" 不然会影响切割
+func (this *Common) GetArgsMap() map[string]string {
+
+	return this.ParseArgs(strings.Join(os.Args, "$$$$"), "$$$$")
+
+}
+
+// ParseArgs 将传入字符串进行解析处理，传入字符串一般是[ -s$$$$xx$$$$-f$$$$filename$$$$--test$$$$aabb]等模式
+func (this *Common) ParseArgs(args string, sep string) map[string]string {
+
+	ret := make(map[string]string)
+
+	var argv []string
+
+	argv = strings.Split(args, sep)
+	for i, v := range argv {
+		if strings.HasPrefix(v, "-") && len(v) == 2 {
+			if i+1 < len(argv) && !strings.HasPrefix(argv[i+1], "-") {
+				ret[v[1:]] = argv[i+1]
+			}
+		}
+
+	}
+	for i, v := range argv {
+		if strings.HasPrefix(v, "-") && len(v) == 2 {
+			if i+1 < len(argv) && strings.HasPrefix(argv[i+1], "-") {
+				ret[v[1:]] = "1"
+			} else if i+1 == len(argv) {
+				ret[v[1:]] = "1"
+			}
+		}
+
+	}
+
+	for i, v := range argv {
+		if strings.HasPrefix(v, "--") && len(v) > 3 {
+			if i+1 < len(argv) && !strings.HasPrefix(argv[i+1], "--") {
+				ret[v[2:]] = argv[i+1]
+			}
+		}
+
+	}
+	for i, v := range argv {
+		if strings.HasPrefix(v, "--") && len(v) > 3 {
+			if i+1 < len(argv) && strings.HasPrefix(argv[i+1], "--") {
+				ret[v[2:]] = "1"
+			} else if i+1 == len(argv) {
+				ret[v[2:]] = "1"
+			}
+		}
+
+	}
+	for k, v := range ret {
+		if k == "file" && this.IsExist(v) {
+			ret[k] = this.ReadFile(v)
+		}
+	}
+	return ret
+
+}
+
+// IsExist 方法用于判断文件是否存在
+func (this *Common) IsExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+// GetLocalIP 获取本地IP，先获取本地所有ip信息，并遍历每一个ip，如果ip中有10|172开头的ip直接返回；
+// 否则，就返回127.0.0.1
+func (this *Common) GetLocalIP() string {
+
+	ips := this.GetAllIps()
+	for _, v := range ips {
+		if strings.HasPrefix(v, "10.") || strings.HasPrefix(v, "172.") || strings.HasPrefix(v, "172.") {
+			return v
+		}
+	}
+	return "127.0.0.1"
+
+}
+
+// GetAllIps 获取主机的所有IP信息
+func (this *Common) GetAllIps() []string {
+	ips := []string{}
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		panic(err)
+	}
+	for _, addr := range addrs {
+		ip := addr.String()
+		pos := strings.Index(ip, "/")
+		if match, _ := regexp.MatchString("(\\d+\\.){3}\\d+", ip); match {
+			if pos != -1 {
+				ips = append(ips, ip[0:pos])
+			}
+		}
+	}
+	return ips
+}
+
+// Home 返回当前用户的家目录
+func (this *Common) Home() (string, error) {
+	user, err := user.Current()
+	if nil == err {
+		return user.HomeDir, nil
+	}
+
+	if "windows" == runtime.GOOS {
+		return this.homeWindows()
+	}
+
+	return this.homeUnix()
+}
+
+// homeWindows获取windows用户家目录
+func (this *Common) homeWindows() (string, error) {
+	// 获取家目录磁盘盘符 C:   内置的环境变量
+	drive := os.Getenv("HOMEDRIVE")
+	// 获取用户家目录  内置的环境变量
+	path := os.Getenv("HOMEPATH")
+	home := drive + path
+	if drive == "" || path == "" {
+		// 获取用户家目录，内置环境变量
+		home = os.Getenv("USERPROFILE")
+	}
+	if home == "" {
+		return "", errors.New("HOMEDRIVE, HOMEPATH, and USERPROFILE are blank")
+	}
+
+	return home, nil
+}
+
+// homeUnix获取linux系统当前用户家目录
+func (this *Common) homeUnix() (string, error) {
+	// First prefer the HOME environmental variable
+	if home := os.Getenv("HOME"); home != "" {
+		return home, nil
+	}
+
+	// If that fails, try the shell
+	var stdout bytes.Buffer
+	cmd := exec.Command("sh", "-c", "eval echo ~$USER")
+	cmd.Stdout = &stdout
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	result := strings.TrimSpace(stdout.String())
+	if result == "" {
+		return "", errors.New("blank output when reading home directory")
+	}
+
+	return result, nil
+}
+
+// GetProductUUID 获取本节点的UUID
+func (this *Common) GetProductUUID() string {
+
+	if "windows" == runtime.GOOS {
+		uuid := this.windowsProductUUID()
+		return uuid
+	}
+
+	filename := "/usr/local/gmd/machine_id"
+	uuid := this.ReadFile(filename)
+	if !this.IsExist("/usr/local/gmd/") {
+		os.Mkdir("/usr/local/gmd/", 0744)
+	}
+	if uuid == "" {
+		uuid := this.GetUUID()
+		this.WriteFile(filename, uuid)
+	}
+	return strings.Trim(uuid, "\n ")
+
+}
+
+// windowsProductUUID方法是在windows系统下先判断是否在用户家目录下存在.machine_id文件
+// 如果存在，则判断里面是否存在本机的uuid，如果没有则获取本机uuid再写入文件中
+func (this *Common) windowsProductUUID() string {
+	user, err := user.Current()
+	if err != nil {
+		log.Debug(err)
+		return ""
+	}
+
+	filename := user.HomeDir + "/.machine_id"
+	var uuid string
+	if !this.IsExist(filename) {
+		uuid = this.GetUUID()
+		this.WriteFile(filename, uuid)
+		return uuid
+	}
+
+	uuid = this.ReadFile(filename)
+
+	if uuid == "" {
+		uuid = this.GetUUID()
+		this.WriteFile(filename, uuid)
+		return uuid
+	}
+
+	return strings.Trim(uuid, "\n")
+}
+
+// GetUUID 获取随机生成的UUID
+func (this *Common) GetUUID() string {
+
+	b := make([]byte, 48)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+	id := this.MD5(base64.URLEncoding.EncodeToString(b))
+	return fmt.Sprintf("%s-%s-%s-%s-%s", id[0:8], id[8:12], id[12:16], id[16:20], id[20:])
+
+}
+
+// MD5 输出字符串的md5值
+func (this *Common) MD5(str string) string {
+
+	md := md5.New()
+	md.Write([]byte(str))
+	return fmt.Sprintf("%x", md.Sum(nil))
+}
+
+// ReadFile 将文件内容读取出来并返回
+func (this *Common) ReadFile(path string) string {
+	if this.IsExist(path) {
+		fi, err := os.Open(path)
+		if err != nil {
+			return ""
+		}
+		defer fi.Close()
+		fd, err := ioutil.ReadAll(fi)
+		return string(fd)
+	} else {
+		return ""
+	}
+}
+
+// WriteFile 把输入参数的内容变量写到文件中；如果存在文件，则先删除后创建；如果不存在则直接创建
+func (this *Common) WriteFile(path string, content string) bool {
+	var f *os.File
+	var err error
+	if this.IsExist(path) {
+		err = os.Remove(path)
+		if err != nil {
+			return false
+		}
+		f, err = os.Create(path)
+	} else {
+		f, err = os.Create(path)
+	}
+
+	if err == nil {
+		defer f.Close()
+		if _, err = io.WriteString(f, content); err == nil {
+			//log.Debug(err)
+			return true
+		} else {
+			return false
+		}
+	} else {
+		//log.Warn(err)
+		return false
 	}
 
 }
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,11 +1,23 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	log "github.com/cihub/seelog"
+	filedriver "github.com/goftp/file-driver"
+	"github.com/goftp/server"
+	"strconv"
+)
 
 type Gmd struct {
 	Util Common
 }
 
+// 封装fmt.Println函数
+func print(args ...interface{}) {
+	fmt.Println(args)
+}
+
+// Help 介绍gmd工具的基本使用方法
 func (this *Gmd) Help(module string, action string) {
 	resp := `
     #############   shell相关   #################
@@ -13,5 +25,61 @@ func (this *Gmd) Help(module string, action string) {
     echo hello | gmd upper     ## 字符串转大写
     echo HELLO | gmd lower     ## 字符串转小写
 `
-	fmt.Println(resp)
+	print(resp)
+}
+
+// Ftpserver
+/*
+使用方法是：gmd ftpserver -u username -p password -h hostIP -P port -d path
+默认值为 -u:root  -p:本机UUID  -h:本地IP   -P:2121 -d:用户家目录
+作用是：在本地使用用户家目录或者指定目录起一个ftp服务
+*/
+func (this *Gmd) Ftpserver(module string, action string) {
+
+	argv := this.Util.GetArgsMap()
+	user := "root"
+	pass := this.Util.GetProductUUID()
+	host := this.Util.GetLocalIP()
+	port := 2121
+	root := "/"
+	if v, err := this.Util.Home(); err == nil {
+		root = v
+	}
+	if v, ok := argv["u"]; ok {
+		user = v
+	}
+	if v, ok := argv["h"]; ok {
+		host = v
+	}
+	if v, ok := argv["p"]; ok {
+		pass = v
+	}
+	if v, ok := argv["P"]; ok {
+		port, _ = strconv.Atoi(v)
+
+	}
+
+	if v, ok := argv["d"]; ok {
+		root = v
+	}
+
+	factory := &filedriver.FileDriverFactory{
+		RootPath: root,
+		Perm:     server.NewSimplePerm("user", "group"),
+	}
+
+	opts := &server.ServerOpts{
+		Factory:  factory,
+		Port:     port,
+		Hostname: host,
+		Auth:     &server.SimpleAuth{Name: user, Password: pass},
+	}
+
+	ftp := server.NewServer(opts)
+
+	err := ftp.ListenAndServe()
+	if err != nil {
+		log.Error("Error starting ftp server:", err)
+	}
+
 }

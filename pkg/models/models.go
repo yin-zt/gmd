f34796425f040ffb2cbc8008816ec7a1369604af
yin-zt <15626499421@163.com>
2023-02-25 11:41:31 +0800

    增加kv和stdinjson两个方法

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,11 +1,17 @@
 package models
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	log "github.com/cihub/seelog"
 	filedriver "github.com/goftp/file-driver"
 	"github.com/goftp/server"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"os"
 	"strconv"
+	"strings"
 )
 
 type Gmd struct {
@@ -24,6 +30,13 @@ func (this *Gmd) Help(module string, action string) {
     echo hello | gmd len       ## 字符串长度
     echo hello | gmd upper     ## 字符串转大写
     echo HELLO | gmd lower     ## 字符串转小写
+
+    #############   file相关    #################
+    gmd fileserver -u username -p password -h hostIP -P port -d path
+
+    #############   日常使用相关  #################
+    gmd kv -k aa -v '{\"a1\": 123, \"b1\": 456}'
+
 `
 	print(resp)
 }
@@ -82,4 +95,96 @@ func (this *Gmd) Ftpserver(module string, action string) {
 		log.Error("Error starting ftp server:", err)
 	}
 
+}
+
+// Kv 执行方式：go run .\main.go kv -k aa -v '{\"a1\": 123, \"b1\": 456}'
+// 数据将会使用leveldb组件存储在用户家目录的o.db下
+func (this *Gmd) Kv(module string, action string) {
+	var (
+		home     string
+		body     map[string]string
+		filename string
+		err      error
+		db       *leveldb.DB
+		k        string
+		v        string
+		data     []byte
+		obj      interface{}
+	)
+
+	body = this.Util.GetArgsMap()
+	fmt.Println(body)
+
+	if _v, ok := body["k"]; ok {
+		k = _v
+	} else {
+		fmt.Println("(error) -k(key) require")
+		return
+	}
+
+	if _v, ok := body["v"]; ok {
+		v = _v
+		if v == "1" || v == "" {
+			obj, v = this.StdinJson(module, action)
+		}
+
+		fmt.Println(v)
+		if err = json.Unmarshal([]byte(v), &obj); err != nil {
+			log.Error(err)
+			return
+		}
+	}
+	fmt.Println("llllllllllll")
+
+	if home, err = this.Util.Home(); err != nil {
+		home = "./"
+	}
+	opts := &opt.Options{
+		CompactionTableSize: 1024 * 1024 * 20,
+		WriteBuffer:         1024 * 1024 * 20,
+	}
+	filename = home + "/" + "o.db"
+
+	db, err = leveldb.OpenFile(filename, opts)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	if v == "" {
+		data, err = db.Get([]byte(k), nil)
+		fmt.Println(err)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		fmt.Println(string(data))
+	} else {
+		err = db.Put([]byte(k), []byte(v), nil)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		fmt.Println("ok")
+
+	}
+
+}
+
+// StdinJson 使用方法是：echo "helloworld \n wocao" | gmd StdinJson  //但不会有输出
+// 作用获取用户输出，并将输入内容连成字符串返回，并不直接调用，而是由其他函数调用
+func (this *Gmd) StdinJson(module string, action string) (interface{}, string) {
+
+	var lines []string
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		lines = append(lines, input.Text())
+	}
+	in := strings.Join(lines, "")
+	var obj interface{}
+	if err := json.Unmarshal([]byte(in), &obj); err != nil {
+		log.Error(err, in)
+		obj = nil
+	}
+	return obj, in
 }

e40b4a8b9caa4d91c0b751058eec1ab1a3951ffd
yin-zt <15626499421@163.com>
2023-03-12 18:37:47 +0800

    add client function

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ I create these  programs as experiments to play with golang, or to solve problem
 ## file
 1. ftpserver: supports you to deploy an ftp server for sharing filedata
    `gmd ftpserver -u mailong -p 123 -P 9090 -h 127.0.0.1`
+2. Httpserver: deploy a http server to share some files in a given floder.
+   `gmd httpserver -h 127.0.0.1 -p 8080`
 
 
 ## web request
@@ -26,6 +28,33 @@ I create these  programs as experiments to play with golang, or to solve problem
    `gmd uuid`
 6. randint: generate one number between a given interval.
    `gmd randint -r 10:40`
+7. md5: support generate md5 value of one string or file content.
+   `gmd md5 -s "string" || gmd md5 -f filename`
+8. cut: support get sub string from a given string.
+   `gmd cut -s "abcdefgf" -p "2:5"`
+9. split: support split one string with given "separator".
+   `echo "hello world" | gmd split -s " "`
+10. replace: support replace old string with given substring.
+    `gmd replace -s worldhello -n FUCK -o world`
+11. match: support find out substring which satisfy regex.
+    `gmd match -s "hell(i)45oworld" -m "[\d+]+" -o "i";`
+12. pq: `gmd pq -m html -f xxx.html`
+
+## interactive operation
+1. keys: `echo '{"aa": "bb", "test": "hello world"}' | gmd keys`
+2. len: `echo "aabbcc" | gmd len  or  echo '{"key1": "val1", "key2": "val2"}' | gmd len`
+3. kvs: `echo [k1, k2, k3] | gmd kvs  || echo '{"k1": "v1", "k2": "v2"}' | gmd kvs`
+4. join: `echo '["aa", "bb", "cc"]' | gmd join -s "-" -w "GG"`
+5. json_val: `echo '{"tt":"helloworld", "bb": "fufu"}' | gmd json_val`
+6. jq: `echo '{"tt":"helloworld", "bb": "fufu"}' | gmd jq`
+7. 
+
+## sql operation
+1. sqlite3: 
+   `gmd sqlite -s sql -f filename -t tablename`
+   `gmd jf -c * -w "condition" --limit 10`
+2. 
+
 
 ## info
 1. info: output information about gmd including version, server info and etc. you can use bellow command:
--- pkg/models/func.go
+++ pkg/models/func.go
@@ -3,8 +3,10 @@ package models
 import (
 	"bufio"
 	"crypto/tls"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/astaxie/beego/httplib"
 	log "github.com/cihub/seelog"
 	filedriver "github.com/goftp/file-driver"
@@ -15,6 +17,7 @@ import (
 	"github.com/yin-zt/gmd/pkg/config"
 	"github.com/yin-zt/gmd/pkg/utils"
 	random "math/rand"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
@@ -163,6 +166,47 @@ func (this *Gmd) Ftpserver(module string, action string) {
 	err := ftp.ListenAndServe()
 	if err != nil {
 		log.Error("Error starting ftp server:", err)
+	}
+
+}
+
+// Httpserver 使用方法是：gmd httpserver -h host -p port -d path
+// 默认host为10或172开头的ip，如果没有则使用127.0.0.1；port默认为8000；d默认为用户家目录
+// 作用是将用户家目录或者指定目录下的文件以http服务的方式提供出去给人访问或下载
+// 下载方式可以使用curl: http://path/filename -o outputfile
+func (this *Gmd) Httpserver(module string, action string) {
+
+	argv := this.Util.GetArgsMap()
+
+	host := this.Util.GetLocalIP()
+	port := "8000"
+	root := "/"
+	if v, err := this.Util.Home(); err == nil {
+		root = v
+	}
+
+	if v, ok := argv["p"]; ok {
+		port = v
+
+	}
+
+	if v, ok := argv["h"]; ok {
+		host = v
+
+	}
+
+	if v, ok := argv["d"]; ok {
+		root = v
+	}
+
+	// 设置http服务的根目录
+	h := http.FileServer(http.Dir(root))
+	fmt.Println(fmt.Sprintf("http server listen %s:%s", host, port))
+	err := http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), h)
+
+	if err != nil {
+		log.Error("Error starting http server:", err)
+		fmt.Println(err)
 	}
 
 }
@@ -455,6 +499,137 @@ func (this *Gmd) Randint(module string, action string) {
 
 }
 
+// Md5 使用方法是：gmd md5 -s "string" || gmd md5 -f filename
+// 目的是输出字符串string和filename文件的md5值
+func (this *Gmd) Md5(module string, action string) {
+
+	s := ""
+	fn := ""
+	argv := this.Util.GetArgsMap()
+	if v, ok := argv["s"]; ok {
+		s = v
+		fmt.Println(this.Util.MD5(s))
+		return
+	}
+	if v, ok := argv["f"]; ok {
+		fn = v
+	}
+	if fn != "" {
+		fmt.Println(this.Util.MD5File(fn))
+		return
+	}
+	_, s = this.StdinJson(module, action)
+	fmt.Println(this.Util.MD5(s))
+
+}
+
+// Cut 使用方法：gmd cut -s "abcdefgf" -p "2:5" | echo "longstr" | cut -p "2:-1"
+// 作用是按照指定的切割间隔，切割指定字符串
+// 支持用户交互输入切割字符串
+func (this *Gmd) Cut(module string, action string) {
+
+	s := ""
+	start := 0
+	end := -1
+	argv := this.Util.GetArgsMap()
+	if v, ok := argv["s"]; ok {
+		s = v
+	}
+	if s == "" {
+		s = this.StdioStr(module, action)
+
+	}
+	if v, ok := argv["p"]; ok {
+		ss := strings.Split(v, ":")
+		if len(ss) == 2 {
+			start, _ = strconv.Atoi(ss[0])
+			if ss[1] != "" {
+				end, _ = strconv.Atoi(ss[1])
+			} else {
+				end = len(s)
+			}
+		}
+		if len(ss) == 1 {
+			start, _ = strconv.Atoi(ss[0])
+			end = len(s)
+		}
+
+	} else {
+		end = len(s)
+	}
+
+	fmt.Println(s[start:end])
+
+}
+
+// Split 使用方法： echo "hello world" | gmd split -s " "
+// 将输入字符串按照给定的字符进行分隔
+// 支持用户交付输入切割字符串
+func (this *Gmd) Split(module string, action string) {
+
+	s := ""
+	sep := ","
+
+	argv := this.Util.GetArgsMap()
+
+	if v, ok := argv["s"]; ok {
+		sep = v
+	}
+
+	if s == "" {
+		s = this.StdioStr(module, action)
+
+	}
+
+	if reg, err := regexp.Compile(sep); err == nil {
+		ss := reg.Split(s, -1)
+		fmt.Println(this.Util.JsonEncodePretty(ss))
+	} else {
+		fmt.Println(s)
+	}
+
+}
+
+// StdioStr 的作用是获取用户输入，并组成字符串输出
+func (this *Gmd) StdioStr(module string, action string) string {
+	var lines []string
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		lines = append(lines, input.Text())
+	}
+	return strings.Join(lines, "\n")
+
+}
+
+// Replace 使用方法是：gmd replace -s worldhello -n FUCK -o world
+// 将输入字符串s中的子串(o,也即-o参数的值)全部替换为 -n参数的值
+func (this *Gmd) Replace(module string, action string) {
+
+	o := ""
+	s := ""
+	n := ""
+
+	argv := this.Util.GetArgsMap()
+
+	if v, ok := argv["o"]; ok {
+		o = v
+	}
+	if v, ok := argv["s"]; ok {
+		s = v
+	}
+	if v, ok := argv["n"]; ok {
+		n = v
+	}
+
+	if s == "" {
+		s = this.StdioStr(module, action)
+		//_, s = this.StdinJson(module, action)
+
+	}
+	reg := regexp.MustCompile(o)
+	fmt.Println(reg.ReplaceAllString(s, n))
+}
+
 // Shell 使用方式：gmd shell  -d path -f file -t 12 -u -a -x
 // 如果本地存在fpath文件(path+file); 且没有位置参数-u；则执行fpath + 其他位置参数组成的命令
 func (this *Gmd) Shell(module string, action string) {
@@ -654,4 +829,670 @@ func (this *Gmd) Shell(module string, action string) {
 	// 本地执行脚本cmds并输出结果
 	result, _, _ = this.Util.Exec(cmds, timeout, nil)
 	fmt.Println(result)
+}
+
+// Match 使用方法是：gmd match -s "hell(i)45oworld" -m "[\d+]+" -o "i";
+// -o 可搭配值为[i|a|m]; 其中i表示只匹配一个，且会在 -m 值前面加上 (?i)表示不区分大小写; a和m表示遍历字符串匹配所有
+// 作用是正则匹配，注意 -m 参数的值不能以"$"结尾，不然影响切割
+func (this *Gmd) Match(module string, action string) {
+
+	m := ""
+	o := ""
+	s := ""
+
+	is_all := false
+
+	argv := this.Util.GetArgsMap()
+	fmt.Println(argv)
+	if v, ok := argv["m"]; ok {
+		m = v
+	}
+	if v, ok := argv["o"]; ok {
+		o = v
+	}
+	if v, ok := argv["s"]; ok {
+		s = v
+	}
+
+	if s == "" {
+		_, s = this.StdinJson(module, action)
+
+	}
+	for i := 0; i < len(o); i++ {
+		if string(o[i]) == "i" {
+			m = "(?i)" + m
+		}
+		if string(o[i]) == "a" || string(o[i]) == "m" {
+			is_all = true
+		}
+	}
+
+	if reg, err := regexp.Compile(m); err == nil {
+
+		if is_all {
+			ret := reg.FindAllString(s, -1)
+			if len(ret) > 0 {
+				fmt.Println(this.Util.JsonEncodePretty(ret))
+			} else {
+				fmt.Println("")
+			}
+		} else {
+			ret := reg.FindString(s)
+			fmt.Println(ret)
+		}
+
+	}
+}
+
+// Keys 使用方法是：echo '{"aa": "bb", "test": "hello world"}' | gmd keys
+// 作用是将传入的类字典字符串，以字典的格式输出，但输出变量依然是字符串
+func (this *Gmd) Keys(module string, action string) {
+
+	obj, _ := this.StdinJson(module, action)
+	fmt.Println(obj)
+	var keys []string
+	switch obj.(type) {
+	case map[string]interface{}:
+		for k, _ := range obj.(map[string]interface{}) {
+			keys = append(keys, k)
+		}
+	}
+	fmt.Println(this.Util.JsonEncodePretty(keys))
+}
+
+// Len 使用方法是：echo "aabbcc" | gmd len  或者  echo '{"key1": "val1", "key2": "val2"}' | gmd len
+// 作用是返回输出字符串长度或者类字典数据中key的个数
+func (this *Gmd) Len(module string, action string) {
+
+	obj, in := this.StdinJson(module, action)
+	switch obj.(type) {
+	case []interface{}:
+		fmt.Println(len(obj.([]interface{})))
+		return
+
+	case map[string]interface{}:
+		i := 0
+		for _ = range obj.(map[string]interface{}) {
+			i = i + 1
+		}
+		fmt.Println(i)
+		return
+	}
+
+	fmt.Println(len(in))
+}
+
+// Kvs 使用方法是：echo [k1, k2, k3] | gmd kvs  || echo '{"k1": "v1", "k2": "v2"}' | gmd kvs
+// echo '{"aa": {"test":"helloworld"}, "bb": {"key1": "value"}}' | gmd kvs
+// 作用是将输入的列表样式的字符串或者字典样式的字符串，转换为显示友好的字典样式
+func (this *Gmd) Kvs(module string, action string) {
+
+	obj, _ := this.StdinJson(module, action)
+	var keys []string
+	switch obj.(type) {
+	case map[string]interface{}:
+		for k, v := range obj.(map[string]interface{}) {
+			switch v.(type) {
+			case map[string]interface{}, []interface{}:
+				if b, e := json.Marshal(v); e == nil {
+					s := strings.Replace(string(b), "\\", "\\\\", -1)
+					s = strings.Replace(s, "\"", "\\\"", -1)
+					keys = append(keys, fmt.Sprintf(k+"=\"%s\"", s))
+				}
+			default:
+				keys = append(keys, fmt.Sprintf(k+"=\"%s\"", v))
+			}
+		}
+	case []interface{}:
+		for i, v := range obj.([]interface{}) {
+
+			keys = append(keys, fmt.Sprintf("a%d=\"%s\"", i, v))
+
+		}
+
+	}
+	fmt.Println(strings.Join(keys, "\n"))
+}
+
+// Join 使用方法是 echo '["aa", "bb", "cc"]' | gmd join -s "-" -w "GG"
+// 作用是将输出的字符串列表使用指定字符拼接，-w表示列表元素拼接前在其前后端加上val，-t参数目前没有作用
+func (this *Gmd) Join(module string, action string) {
+
+	obj, _ := this.StdinJson(module, action)
+	sep := ","
+	wrap := ""
+	trim := ""
+	argv := this.Util.GetArgsMap()
+	if v, ok := argv["s"]; ok {
+		sep = v
+	}
+	if v, ok := argv["t"]; ok {
+		trim = v
+	}
+	if v, ok := argv["w"]; ok {
+		wrap = v
+	}
+	var lines []string
+	switch obj.(type) {
+	case []interface{}:
+		for _, v := range obj.([]interface{}) {
+			if trim != "" {
+				if v == nil || fmt.Sprintf("%s", v) == "" {
+					continue
+				}
+			}
+			if wrap != "" {
+				lines = append(lines, fmt.Sprintf("%s%s%s", wrap, v, wrap))
+			} else {
+				lines = append(lines, fmt.Sprintf("%s", v))
+			}
+		}
+	}
+	fmt.Println(strings.Join(lines, sep))
+
+}
+
+// Sqlite 使用方法 gmd sqlite -s sql -f filename -t tablename
+// 目前这个命令只适配sqlite数据库的的操作
+func (this *Gmd) Sqlite(module string, action string) {
+	var (
+		err      error
+		v        interface{}
+		s        string
+		isUpdate bool
+		s2       string
+		rows     []map[string]interface{}
+		count    int64
+		db       *sql.DB
+	)
+
+	f := ""
+	t := "data"
+	h := `
+   -s sql
+   -f filename
+   -t tablename
+`
+	argv := this.Util.GetArgsMap()
+	if v, ok := argv["f"]; ok {
+		f = v
+	}
+
+	if _, ok := argv["h"]; ok || len(argv) == 0 {
+		fmt.Println(h)
+		return
+	}
+	if v, ok := argv["t"]; ok {
+		t = v
+	}
+	if v, ok := argv["s"]; ok {
+		s = v
+	}
+	if s == "" {
+		v, s = this.StdinJson(module, action)
+	}
+	_ = s
+	if s == "" {
+		fmt.Println("(error) -s input null")
+		return
+	}
+
+	// 此操作是判断sql执行语句是select还是update操作
+	s2 = strings.TrimSpace(strings.ToLower(s))
+	if strings.HasPrefix(s2, "select") {
+		isUpdate = false
+	}
+
+	if v != nil {
+		if db, err = this.Util.SqliteInsert(f, t, v.([]interface{})); err != nil {
+			log.Error(err)
+			fmt.Println(err)
+			return
+		}
+		_ = db
+		fmt.Println("ok")
+		return
+	}
+	if !isUpdate {
+
+		count, err = this.Util.SqliteExec(f, s)
+
+		if err != nil {
+			log.Error(err)
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(fmt.Sprintf("ok(%d)", count))
+
+	} else {
+		rows, err = this.Util.SqliteQuery(f, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(this.Util.JsonEncodePretty(rows))
+	}
+}
+
+// Pq 使用方法是 gmd pq -m html -f xxx.html
+// 目的是查询html文件中所有href属性的值，并将每个html中的href组件的链接和内容以字典方式进入列表中返回
+func (this *Gmd) Pq(module string, action string) {
+	var (
+		err   error
+		dom   *goquery.Document
+		title string
+		href  string
+		ok    bool
+		text  string
+		html  string
+	)
+	fmt.Println(ok)
+	u := ""
+	s := "html"
+	m := "text"
+	f := ""
+	c := ""
+	a := "link"
+
+	argv := this.Util.GetArgsMap()
+	if v, ok := argv["f"]; ok {
+		f = v
+	}
+	if v, ok := argv["s"]; ok {
+		s = v
+	}
+	if v, ok := argv["m"]; ok {
+		m = v
+	}
+
+	if v, ok := argv["a"]; ok {
+		a = v
+	}
+
+	if f != "" {
+
+		c = this.Util.ReadFile(f)
+
+	}
+
+	if c == "" {
+		var lines []string
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			lines = append(lines, input.Text())
+		}
+		c = strings.Join(lines, "")
+	}
+
+	_ = c
+	_ = m
+	_ = s
+	_ = u
+	_ = err
+
+	dom, err = goquery.NewDocumentFromReader(strings.NewReader(c))
+
+	var result []interface{}
+	dom.Find(s).Each(func(i int, selection *goquery.Selection) {
+
+		if a == "link" {
+			href, ok = selection.Attr("href")
+			title = selection.Text()
+			item := make(map[string]string)
+			item["href"] = strings.TrimSpace(href)
+			item["title"] = strings.TrimSpace(title)
+			result = append(result, item)
+		} else if a == "table" {
+			var rows []string
+			selection.Find("table tr").Each(func(i int, selection *goquery.Selection) {
+				var row []string
+				selection.Find("td").Each(func(i int, selection *goquery.Selection) {
+					row = append(row, strings.TrimSpace(selection.Text()))
+				})
+				rows = append(rows, strings.Join(row, "######"))
+			})
+
+			result = append(result, strings.Join(rows, "$$$$$"))
+
+		} else {
+
+			if m == "text" {
+				text = selection.Text()
+				result = append(result, text)
+			}
+			if m == "html" {
+				fmt.Println(123)
+				html, err = selection.Html()
+				result = append(result, html)
+			}
+		}
+	})
+
+	fmt.Println(this.Util.JsonEncodePretty(result))
+
+}
+
+// Json_val 使用方法是 echo '{"tt":"helloworld", "bb": "fufu"}' | gmd json_val
+// 直接调用jq方法
+func (this *Gmd) Json_val(module string, action string) {
+	this.Jq(module, action)
+}
+
+// Jq 使用方法是echo '{"tt":"helloworld", "bb": "fufu"}' | gmd jq
+// json解析器，-k json中的key，嵌套时使用逗号分隔，如 -k data,rows,ip
+func (this *Gmd) Jq(module string, action string) {
+
+	data, _ := this.StdinJson(module, action)
+	if data == nil {
+		return
+	}
+	key := ""
+	var obj interface{}
+	argv := this.Util.GetArgsMap()
+	if v, ok := argv["k"]; ok {
+		key = v
+	}
+	var ks []string
+	if strings.Contains(key, ",") {
+		ks = strings.Split(key, ",")
+	} else {
+		ks = strings.Split(key, ".")
+	}
+
+	obj = data
+
+	// 解析传入data的接口字符串中，是否为字典类型，如果是则查找是否字典中有key这个键，有则返回key对应的值；
+	// 如果不是字典类型，则返回nil
+	ParseDict := func(obj interface{}, key string) interface{} {
+		switch obj.(type) {
+		case map[string]interface{}:
+			if v, ok := obj.(map[string]interface{})[key]; ok {
+				return v
+			}
+		}
+		return nil
+
+	}
+
+	// 判断传入的obj接口类型的值是否为列表样式，如果是则继续判断key是数字样式还是字符串样式，
+	// 如果是数字样式则，返回列表中索引下标为key的值；如果是字符串样式，则返回遍历列表中所有元素()
+	ParseList := func(obj interface{}, key string) interface{} {
+		var ret []interface{}
+		switch obj.(type) {
+		case []interface{}:
+			if ok, _ := regexp.MatchString("^\\d+$", key); ok {
+				i, _ := strconv.Atoi(key)
+				return obj.([]interface{})[i]
+			}
+
+			for _, v := range obj.([]interface{}) {
+				switch v.(type) {
+				case map[string]interface{}:
+					if key == "*" {
+						for _, vv := range v.(map[string]interface{}) {
+							ret = append(ret, vv)
+						}
+					} else {
+						if vv, ok := v.(map[string]interface{})[key]; ok {
+							ret = append(ret, vv)
+						}
+					}
+				case []interface{}:
+					if key == "*" {
+						for _, vv := range v.([]interface{}) {
+							ret = append(ret, vv)
+						}
+					} else {
+						ret = append(ret, v)
+					}
+				}
+			}
+		}
+		return ret
+	}
+	if key != "" {
+		for _, k := range ks {
+			switch obj.(type) {
+			case map[string]interface{}:
+				obj = ParseDict(obj, k)
+			case []interface{}:
+				obj = ParseList(obj, k)
+			}
+		}
+	}
+
+	switch obj.(type) {
+	case map[string]interface{}, []interface{}:
+		fmt.Println(this.Util.JsonEncodePretty(obj))
+	default:
+		fmt.Println(obj)
+	}
+
+}
+
+// Jf 用法：gmd jf -c * -w "condition" --limit 10
+// json 数组过滤器  -w sql中的where条件 -c sql中的column
+// 此命令只适配sqlite3数据库
+func (this *Gmd) Jf(module string, action string) {
+
+	data, _ := this.StdinJson(module, action)
+
+	c := "*"
+	w := "1=1"
+	s := "select %s from data where 1=1 and %s %s"
+
+	limit := ""
+	argv := this.Util.GetArgsMap()
+	if v, ok := argv["c"]; ok {
+		c = v
+	}
+
+	if v, ok := argv["w"]; ok {
+		w = v
+	}
+
+	if v, ok := argv["limit"]; ok {
+		limit = " limit " + v
+	}
+
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Error(err)
+		log.Flush()
+		return
+	}
+
+	Push := func(db *sql.DB, records []interface{}) error {
+		hashKeys := map[string]struct{}{}
+
+		keyword := []string{"ALTER",
+			"CLOSE",
+			"COMMIT",
+			"CREATE",
+			"DECLARE",
+			"DELETE",
+			"DENY",
+			"DESCRIBE",
+			"DOMAIN",
+			"DROP",
+			"EXECUTE",
+			"EXPLAN",
+			"FETCH",
+			"GRANT",
+			"INDEX",
+			"INSERT",
+			"OPEN",
+			"PREPARE",
+			"PROCEDURE",
+			"REVOKE",
+			"ROLLBACK",
+			"SCHEMA",
+			"SELECT",
+			"SET",
+			"SQL",
+			"TABLE",
+			"TRANSACTION",
+			"TRIGGER",
+			"UPDATE",
+			"VIEW",
+			"GROUP"}
+
+		_ = keyword
+
+		//for _, record := range records {
+		//	switch record.(type) {
+		//	case map[string]interface{}:
+		//		for key, _ := range record.(map[string]interface{}) {
+		//			key2 := key
+		//			if this.util.Contains(strings.ToUpper(key), keyword) {
+		//				key2 = "_" + key
+		//				record.(map[string]interface{})[key2] = record.(map[string]interface{})[key]
+		//				delete(record.(map[string]interface{}), key)
+		//			}
+		//			hashKeys[key2] = struct{}{}
+		//		}
+		//	}
+		//}
+
+		for _, record := range records {
+			switch record.(type) {
+			case map[string]interface{}:
+				for key, _ := range record.(map[string]interface{}) {
+					if strings.HasPrefix(key, "`") {
+						continue
+					}
+					key2 := fmt.Sprintf("`%s`", key)
+					record.(map[string]interface{})[key2] = record.(map[string]interface{})[key]
+					delete(record.(map[string]interface{}), key)
+					hashKeys[key2] = struct{}{}
+				}
+			}
+		}
+
+		keys := []string{}
+
+		for key, _ := range hashKeys {
+			keys = append(keys, key)
+		}
+		//		db.Exec("DROP TABLE data")
+		query := "CREATE TABLE data (" + strings.Join(keys, ",") + ")"
+		if _, err := db.Exec(query); err != nil {
+			log.Error(query)
+			log.Flush()
+			return err
+		}
+
+		for _, record := range records {
+			recordKeys := []string{}
+			recordValues := []string{}
+			recordArgs := []interface{}{}
+
+			switch record.(type) {
+			case map[string]interface{}:
+
+				for key, value := range record.(map[string]interface{}) {
+					recordKeys = append(recordKeys, key)
+					recordValues = append(recordValues, "?")
+					recordArgs = append(recordArgs, value)
+				}
+
+			}
+
+			query := "INSERT INTO data (" + strings.Join(recordKeys, ",") +
+				") VALUES (" + strings.Join(recordValues, ", ") + ")"
+
+			statement, err := db.Prepare(query)
+			if err != nil {
+				log.Error(err, "can't prepare query: %s", query, recordKeys, recordArgs, recordValues)
+				log.Flush()
+				continue
+
+			}
+
+			_, err = statement.Exec(recordArgs...)
+			if err != nil {
+				log.Error(
+					err, "can't insert record",
+				)
+
+			}
+			statement.Close()
+		}
+
+		return nil
+	}
+
+	switch data.(type) {
+	case []interface{}:
+		err = Push(db, data.([]interface{}))
+		if err != err {
+			fmt.Println(err)
+			return
+		}
+	default:
+		msg := "(error) just support list"
+		fmt.Println(msg)
+		log.Error(msg)
+		return
+
+	}
+
+	defer db.Close()
+	if err == nil {
+		db.SetMaxOpenConns(1)
+	} else {
+		log.Error(err.Error())
+	}
+
+	s = fmt.Sprintf(s, c, w, limit)
+
+	rows, err := db.Query(s)
+
+	if err != nil {
+		log.Error(err, s)
+		log.Flush()
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	records := []map[string]interface{}{}
+	for rows.Next() {
+		record := map[string]interface{}{}
+
+		columns, err := rows.Columns()
+		if err != nil {
+			log.Error(
+				err, "unable to obtain rows columns",
+			)
+			continue
+		}
+
+		pointers := []interface{}{}
+		for _, column := range columns {
+			var value interface{}
+			pointers = append(pointers, &value)
+			record[column] = &value
+		}
+
+		err = rows.Scan(pointers...)
+		if err != nil {
+			log.Error(err, "can't read result records")
+			continue
+		}
+
+		for key, value := range record {
+			indirect := *value.(*interface{})
+			if value, ok := indirect.([]byte); ok {
+				record[key] = string(value)
+			} else {
+				record[key] = indirect
+			}
+		}
+
+		records = append(records, record)
+	}
+
+	fmt.Println(Util.JsonEncodePretty(records))
+
 }
